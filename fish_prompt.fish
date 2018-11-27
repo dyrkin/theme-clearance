a5ec6e3610c8390296040ca8f965608641f65e95
Eugene Zadyra <untrance@gmail.com>
2018-11-27 09:12:31 +0100

    reformatted code

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -6,62 +6,62 @@
 # - Git branch and dirty state (if inside a git repo)
 
 function _git_branch_name
-  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+    echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
 end
 
 function _git_is_dirty
-  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
+    echo (command git status -s --ignore-submodules=dirty ^/dev/null)
 end
 
 function _remote_hostname
-  if test -n "$SSH_CONNECTION"
-    echo "ssh"
-  end
+    if test -n "$SSH_CONNECTION"
+        echo "ssh"
+    end
 end
 
 function fish_prompt
-  set -l last_status $status
+    set -l last_status $status
 
-  set -l cyan (set_color cyan)
-  set -l yellow (set_color yellow)
-  set -l red (set_color red)
-  set -l blue (set_color blue)
-  set -l green (set_color green)
-  set -l normal (set_color normal)
+    set -l cyan (set_color cyan)
+    set -l yellow (set_color yellow)
+    set -l red (set_color red)
+    set -l blue (set_color blue)
+    set -l green (set_color green)
+    set -l normal (set_color normal)
 
-  set -l cwd $blue(pwd | sed "s:^$HOME:~:")
+    set -l cwd $blue(pwd | sed "s:^$HOME:~:")
 
-  # Output the prompt, left to right
+    # Output the prompt, left to right
 
-  # Add a newline before new prompts
-  echo -e ''
+    # Add a newline before new prompts
+    echo -e ''
 
-  # Display [venvname] if in a virtualenv
-  if set -q VIRTUAL_ENV
-      echo -n -s (set_color -b cyan black) '[' (basename "$VIRTUAL_ENV") ']' $normal ' '
-  end
-
-  # Print pwd or full path
-  echo -n -s $cwd $normal
-
-  # Show git branch and status
-  if [ (_git_branch_name) ]
-    set -l git_branch (_git_branch_name)
-
-    if [ (_git_is_dirty) ]
-      set git_info '(' $yellow $git_branch "±" $normal ')'
-    else
-      set git_info '(' $green $git_branch $normal ')'
+    # Display [venvname] if in a virtualenv
+    if set -q VIRTUAL_ENV
+        echo -n -s (set_color -b cyan black) '[' (basename "$VIRTUAL_ENV") ']' $normal ' '
     end
-    echo -n -s ' · ' $git_info $normal
-  end
 
-  set -l prompt_color $red
-  if test $last_status = 0
-    set prompt_color $normal
-  end
+    # Print pwd or full path
+    echo -n -s $cwd $normal
 
-  # Terminate with a nice prompt char
-  echo -e ''
-  echo -e -n -s (_remote_hostname) ' ' $prompt_color '⟩ ' $normal
+    # Show git branch and status
+    if [ (_git_branch_name) ]
+        set -l git_branch (_git_branch_name)
+
+        if [ (_git_is_dirty) ]
+            set git_info '(' $yellow $git_branch "±" $normal ')'
+        else
+            set git_info '(' $green $git_branch $normal ')'
+        end
+        echo -n -s ' · ' $git_info $normal
+    end
+
+    set -l prompt_color $red
+    if test $last_status = 0
+        set prompt_color $normal
+    end
+
+    # Terminate with a nice prompt char
+    echo -e ''
+    echo -e -n -s (_remote_hostname) ' ' $prompt_color '⟩ ' $normal
 end
