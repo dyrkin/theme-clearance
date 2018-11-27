43517f27425a6e07283587709cce3ba8a5eabcad
Eugene Zadyra <untrance@gmail.com>
2018-11-27 16:29:12 +0100

    Recfotoring

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -20,44 +20,55 @@ set -x blue (set_color blue)
 set -x green (set_color green)
 set -x normal (set_color normal)
 
-function _git_branch_name
-    echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
-end
-
-function _git_is_dirty
-    echo (command git status -s --ignore-submodules=dirty ^/dev/null)
-end
-
-function _remote_hostname
+function ssh::label
     if test -n "$SSH_CONNECTION"
         echo "ssh "
     end
 end
 
-function __git_ahead_verbose -S -d 'Print a more verbose ahead/behind state for the current branch'
+function git::branch_name
+    echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+end
+
+function git::is_dirty
+    echo (command git status -s --ignore-submodules=dirty ^/dev/null)
+end
+
+function git::ahead -S -d 'Print a more verbose ahead/behind state for the current branch'
+    set -l commits (command git rev-list --left-right '@{upstream}...HEAD' 2>/dev/null)
+    or return
+
+    set -l ahead (count (for arg in $commits; echo $arg; end | command grep -v '^<'))
+
+    set -l ahead_msg (echo $red"$git_ahead_glyph$ahead")
+
+    switch "$ahead"
+        case '' # no upstream
+        case '0' # equal to upstream
+            return
+        case '*' # ahead of upstream
+            echo "[$ahead_msg$normal]"
+    end
+end
+
+function git::behind -S -d 'Print a more verbose ahead/behind state for the current branch'
     set -l commits (command git rev-list --left-right '@{upstream}...HEAD' 2>/dev/null)
     or return
 
     set -l behind (count (for arg in $commits; echo $arg; end | command grep '^<'))
-    set -l ahead (count (for arg in $commits; echo $arg; end | command grep -v '^<'))
 
-    set -l ahead_msg (echo $red"$git_ahead_glyph$ahead")
     set -l behind_msg (echo $green"$git_behind_glyph$behind")
 
-    switch "$ahead $behind"
+    switch "$behind"
         case '' # no upstream
-        case '0 0' # equal to upstream
+        case '0' # equal to upstream
             return
-        case '* 0' # ahead of upstream
-            echo "[$ahead_msg$normal]"
-        case '0 *' # behind upstream
+        case '*' # behind upstream
             echo "[$behind_msg$normal]"
-        case '*' # diverged from upstream
-            echo "[$ahead_msg$behind_msg$normal]"
     end
 end
 
-function __git_untracked
+function git::untracked
     set -l show_untracked (command git config --bool bash.showUntrackedFiles 2>/dev/null)
     if [ "$show_untracked" != 'false' ]
         set -l new (command git ls-files --other --exclude-standard --directory --no-empty-directory 2>/dev/null)
@@ -69,12 +80,55 @@ function __git_untracked
     end
 end
 
+function git::staged
+    echo -n (command git diff --cached --no-ext-diff --quiet --exit-code 2>/dev/null; or echo -n "$git_staged_glyph")
+end
+
+function git::stashed
+    echo -n (command git rev-parse --verify --quiet refs/stash >/dev/null; and echo -n "$git_stashed_glyph")
+end
+
+function git::git
+    set -l staged (git::staged)
+    set -l stashed (git::stashed)
+    set -l untracked (git::untracked)
+    set -l ahead (git::ahead)
+    set -l behind (git::behind)
+
+
+    if [ "$staged" ]
+        set ret_val "$ret_val$staged|"
+    end
+
+    if [ "$stashed" ]
+        set ret_val "$ret_val$stashed|"
+    end
+
+    if [ "$untracked" ]
+        set ret_val "$ret_val$untracked|"
+    end
+
+    if [ "$ahead" ]
+        set ret_val "$ret_val$ahead|"
+    end
+
+    if [ "$behind" ]
+        set ret_val "$ret_val$ahead|"
+    end
+
+    set ret_val (string trim -r -c "|" "$ret_val")
+
+    if [ "$ret_val" ]
+        echo "[$ret_val]"
+    else
+        return
+    end
+end
+
 function fish_prompt
     set -l last_status $status
 
     set -l cwd $blue(pwd | sed "s:^$HOME:~:")
-
-    # Output the prompt, left to right
 
     # Add a newline before new prompts
     echo -e ''
@@ -88,24 +142,17 @@ function fish_prompt
     echo -n -s $cwd $normal
 
     # Show git branch and status
-
-    set -l git_branch (_git_branch_name)
+    set -l git_branch (git::branch_name)
 
     if [ "$git_branch" ]
-        set -l staged (command git diff --cached --no-ext-diff --quiet --exit-code 2>/dev/null; or echo -n "$git_staged_glyph")
-        set -l stashed (command git rev-parse --verify --quiet refs/stash >/dev/null; and echo -n "$git_stashed_glyph")
-        set -l untracked (__git_untracked)
-        set -l ahead (__git_ahead_verbose)
-        set -l dirty (_git_is_dirty)
+        set -l is_dirty (git::is_dirty)
 
-        #$staged $stashed $untracked
-        
-        if [ "$dirty" ]
-            set git_info '(' $yellow $git_branch "±" $normal ')'
+        if [ "$is_dirty" ]
+            set git_info '(' $yellow $git_branch $normal ')'
         else
             set git_info '(' $green $git_branch $normal ')'
         end
-        echo -n -s ' · ' $git_info $normal "$ahead"
+        echo -n -s ' · ' $git_info $normal
 
         set -l prompt_color $red
         if test $last_status = 0
@@ -113,7 +160,13 @@ function fish_prompt
         end
     end
 
+    set -l ssh_label (ssh::label)
+
     # Terminate with a nice prompt char
     echo -e ''
-    echo -e -n -s (_remote_hostname) $prompt_color '⟩ ' $normal
+    echo -e -n -s $normal $ssh_label $prompt_color '⟩ ' $normal
+end
+
+function fish_right_prompt
+    echo (git::git)
 end
